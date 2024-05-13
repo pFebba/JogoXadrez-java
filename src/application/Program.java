5c2c1e4c67663f6c25e58e2b79027473b4a61a92
pFebba <pedro.febba@outlook.com>
2024-05-13 11:00:04 -0300

    Printing chess board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ChessMatch cm = new ChessMatch();
+		UI.printBoard(cm.getPieces());
 
 	}
 
