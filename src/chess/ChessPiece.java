5b17f0200d61a4dfe36da5c1c18b70c32ee162f0
pFebba <pedro.febba@outlook.com>
2024-05-27 16:10:42 -0300

    Decrese move count and increase move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece{
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -16,6 +17,18 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}
 	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+	
+	public int moveCount() {
+		return moveCount; 
+	}
+	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return  p != null &&  p.getColor() != color;
@@ -24,6 +37,9 @@ public abstract class ChessPiece extends Piece{
 	public ChessPosition getChessPosition(){
 		return ChessPosition.fromPosition(position);
 	}
+	
+	
+	
 
 
 
